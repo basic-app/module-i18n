8f5441aaad52884c19a078ef9cc4aa12f2e1a3cb
basic-app <dev@basic-app.com>
2019-07-26 09:40:21 +0300

    n/a

--- Config/Events.php
+++ Config/Events.php
@@ -1,16 +1,18 @@
 <?php
 
+use BasicApp\Helpers\Url;
+
 BasicApp\Core\CoreEvents::onPreSystem(function()
 {
 	helper(['t', 'current_lang']);
 });
 
-BasicApp\Admin\AdminEvents::onAdminMainMenu(function($menu)
+CodeIgniter\Events\Events::on('admin_main_menu', function($menu)
 {
     if (BasicApp\I18n\Controllers\Admin\Translation::checkAccess())
     {
         $menu->items['system']['items']['translation'] = [
-            'url' => site_url('admin/translation'),
+            'url' => Url::createUrl('admin/translation'),
             'label' => t('admin.menu', 'Translations'),
             'icon' => 'fa fa-book'
         ];
