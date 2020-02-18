be6c8a2478ac30470b16d5b86ed46e3eb33f3b77
basic-app <dev@basic-app.com>
2020-02-18 21:37:26 +0300

    n/a

--- Config/Routes.php
+++ Config/Routes.php
@@ -4,5 +4,5 @@
  * @license MIT
  * @link http://basic-app.com
  */
-$routes->add('admin/translation', 'BasicApp\I18n\Controllers\Admin\Translation::index');
-$routes->add('admin/translation/(:segment)', 'BasicApp\I18n\Controllers\Admin\Translation::$1');
+$routes->add('admin/translation', '\BasicApp\I18n\Controllers\Admin\Translation::index');
+$routes->add('admin/translation/(:segment)', '\BasicApp\I18n\Controllers\Admin\Translation::$1');
